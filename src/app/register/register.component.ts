d8a69ce42cd7b4ab1bf15461d6af8ee4e5d19b25
ismaeleision <ismaeleision@gmail.com>
2022-04-23 15:55:09 +0200

    body color

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CrudService } from '../service/crud.service';
 
 @Component({
-  selector: 'app-register',
+  selector: 'register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
