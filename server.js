fc67a55b10362a6a3e5506c8e0b359e32340f86f
ismaeleision <ismaeleision@gmail.com>
2022-06-09 13:10:00 +0200

    prueba 6

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 function requireHTTPS(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
   }
   next();
 }
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(requireHTTPS);
 
-app.use(express.static('./dist/<name-on-package.json>/'));
+app.use(express.static("./dist/trabajo/"));
 
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'});
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/trabajo/" });
 });
 
 app.listen(process.env.PORT || 8080);
